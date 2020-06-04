692b15285c64869865f63b6f8b3285c3fc93ce5f
agdsarge <agdsargeant@gmail.com>
2020-06-04 12:02:34 -0400

    finished basic deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  //assign myNode to the submit button
+  //myNode.addEventListener("submit", callbackFunction)
+  //handleSubmit
+  //preventDefault
+  //populate the todoList with .value
+  let taskForm = document.querySelector("#create-task-form");
+  taskForm.addEventListener("submit", handleSubmit)
+
+  function handleSubmit(e) {
+      e.preventDefault();
+      // console.log(e.target)
+      // console.log(e)
+
+      let task = document.querySelector("#new-task-description").value;
+      let deadline = document.querySelector("#new-task-deadline").value;
+      // console.log(task)
+      writeTasktoDOM(task, deadline)
+  }
+
+  function writeTasktoDOM(task, deadline) {
+      let text = `${task}: ${deadline}`
+      let listElement = document.createElement("li")
+      listElement.innerText = text
+      let entireList = document.querySelector("#tasks")
+      entireList.appendChild(listElement)
+  }
+
+
+
 });
